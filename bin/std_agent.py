3338020a1a589e6270468f322602bc6e3a5402eb
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-05-17 15:19:29 -0500

    Simple agent that can read messages from cli
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ bin/std_agent.py
@@ -0,0 +1,20 @@
+import asyncio
+from agent import Agent
+from config import Config
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    config = Config.from_args_file_defaults()
+    agent = loop.run_until_complete(Agent.from_config(config))
+
+    @agent.register_route('testing')
+    async def testing_handler(agent, msg):
+        await agent.outbound_transport.send('to_key', 'from_key', msg)
+
+
+    # Main loop
+    try:
+        loop.run_until_complete(agent.start())
+    except KeyboardInterrupt:
+        print("exiting")
