6ad4bae73b1946f58a86fa5decfb36d9cfc2b15b
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-05-17 15:19:12 -0500

    Fixes in standard transports
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- src/transport/inbound/standard_in.py
+++ src/transport/inbound/standard_in.py
@@ -5,13 +5,15 @@ inbound_queue = asyncio.Queue()
 
 async def start():
     loop = asyncio.get_event_loop()
+    print("Listening on stdin")
     while True:
         msg = ''
         line = await loop.run_in_executor(None, sys.stdin.readline)
         while line != "\n":
             msg += line
             line = await loop.run_in_executor(None, sys.stdin.readline)
-        await inbound_queue.put(msg)
+        if msg:
+            await inbound_queue.put(msg) 
 
 async def recv():
     return await inbound_queue.get()
--- src/transport/outbound/standard_out.py
+++ src/transport/outbound/standard_out.py
@@ -3,5 +3,9 @@ import sys
 
 loop = asyncio.get_event_loop()
 
-async def send(to_key, msg):
-    await loop.run_in_executor(None, sys.stdout.write, to_key, msg)
+async def send(to_key, from_key, msg):
+    out = "To: " + to_key
+    out += "\nFrom: " + from_key
+    out += "\nBody: " + msg.serialize()
+
+    await loop.run_in_executor(None, print, out)
