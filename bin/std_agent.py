f5240ac9d6e1d74f3f63179238de00bea85cc736
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-05-21 15:25:18 -0500

    Don't nest conductor initilization in agent
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

--- bin/std_agent.py
+++ bin/std_agent.py
@@ -1,6 +1,7 @@
 import asyncio
 from agent import Agent
 from config import Config
+from conductor import Conductor
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
@@ -8,10 +9,11 @@ if __name__ == '__main__':
     config = Config.from_args_file_defaults()
 
     agent = loop.run_until_complete(Agent.from_config(config))
+    agent.conductor = Conductor.from_wallet_handle_config(agent.wallet_handle, config)
 
     @agent.route('testing')
     async def testing_handler(agent, msg):
-        await agent.conductor.send('to_key', 'from_key', msg)
+        await agent.conductor.send('to_did', 'to_key', 'from_key', msg)
 
     # Main loop
     try:
