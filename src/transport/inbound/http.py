6af0f2332f920fbb31bbc44b590d61db903e371a
Daniel Bluhm <daniel.bluhm@sovrin.org>
2019-05-21 11:02:49 -0500

    Add HTTP Transport and Connections
    Signed-off-by: Daniel Bluhm <daniel.bluhm@sovrin.org>

---
+++ src/transport/inbound/http.py
@@ -0,0 +1,37 @@
+import aiohttp
+from transport.connection import Connection, ConnectionType
+
+async def accept(loop, connection_queue, **kwargs):
+    routes = [
+        aiohttp.web.get('/indy', post_handle)
+    ]
+    app = aiohttp.web.Application()
+    app['connection_queue'] = connection_queue
+    app.add_routes(routes)
+    runner = aiohttp.web.AppRunner(app)
+    loop.run_until_complete(runner)
+    server = aiohttp.web.TCPSite(runner=runner, port=kwargs['port'])
+    await server.start()
+
+async def post_handle(request):
+    msg = await request.read()
+    conn = HTTPConnection(msg)
+    await request.app['connection_queue'].put(conn)
+    await conn.wait()
+    if conn.new_msg:
+        return aiohttp.web.Response(text=conn.new_msg)
+    else:
+        raise aiohttp.web.HTTPAccepted()
+
+class HTTPConnection(Connection):
+    def __init__(self, msg):
+        super().__init__(ConnectionType.RECV)
+        self.msg = msg
+        self.new_msg = None
+
+    async def recv(self):
+        return self.msg
+
+    async def send(self, new_msg):
+        self.new_msg = new_msg
+        self.close()
---
+++ src/transport/outbound/http.py
@@ -0,0 +1,18 @@
+import aiohttp
+from transport.connection import Connection, ConnectionType
+
+class HTTPOutConnection(Connection):
+    def __init__(self, endpoint):
+        super().__init__(ConnectionType.SEND)
+        self.endpoint = endpoint
+
+    async def send(self, msg):
+        async with aiohttp.ClientSession() as session:
+            headers = {'content-type': 'application/ssi-agent-wire'}
+            async with session.post(self.endpoint, data=msg, headers=headers) as resp:
+                if resp.status != 202:
+                    print(resp.status)
+                    print(await resp.text())
+
+async def open(endpoint):
+    return HTTPOutConnection(endpoint)
